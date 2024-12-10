b974498cdfe100f1a67202410f334dabfb1c2375
Allan <allan.rosendo@hotmail.com>
2024-12-10 11:57:06 -0300

    adding static files and urls/views from contact

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(
+        request,
+        'contact/index.html',
+        {
+            'title': 'Contact',
+            'message': 'Message from the contact page.',
+        }
+    )
