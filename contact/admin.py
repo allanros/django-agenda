de3516ddc793ec419e045d4d1d123d8ca19c336e
Allan <allan.rosendo@hotmail.com>
2024-12-11 10:04:58 -0300

    updating contact models, core settings and migrations

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'created_at',
+    )
+    # list_filter = ('created_at',)
+    ordering = ('-created_at',)
+    search_fields = ('first_name', 'last_name', 'phone', 'email')
+    list_per_page = 5
+    list_max_show_all = 200
+    list_editable = ('phone', )
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d/')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
