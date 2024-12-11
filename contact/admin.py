0628b53af59b66ec3a7152da4dd1eb56cb70acce
Allan <allan.rosendo@hotmail.com>
2024-12-11 13:00:32 -0300

    adding random contacts to project

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact, Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
@@ -8,12 +8,19 @@ class ContactAdmin(admin.ModelAdmin):
         'first_name',
         'last_name',
         'phone',
+        'category',
         'email',
         'created_at',
     )
     # list_filter = ('created_at',)
+    list_display_links = ('id', 'first_name',)
     ordering = ('-created_at',)
     search_fields = ('first_name', 'last_name', 'phone', 'email')
-    list_per_page = 5
+    list_per_page = 15
     list_max_show_all = 200
     list_editable = ('phone', )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    ordering = ('-id',)
--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,17 @@
 from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50, blank=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50, blank=False)
@@ -10,6 +22,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
