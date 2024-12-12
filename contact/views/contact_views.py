18efa9931bd2aae8af85d73cacb2773d77badff8
Allan <allan.rosendo@hotmail.com>
2024-12-12 11:02:46 -0300

    adding index view to contact app

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from contact.models import Contact
 
 def index(request):
+    contacts = Contact.objects.all()
+
     return render(
         request,
         'contact/index.html',
         {
             'title': 'Contact',
             'message': 'Message from the contact page.',
+            'contacts': contacts
         }
     )
